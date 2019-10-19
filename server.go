d2c2e6a9bb43444ce3ae040072d86c2ab8fc5a4f
Ram <ram@Apekshas-MacBook-Air.local>
2019-10-19 00:36:27 -0400

    gin server

--- server.go
+++ server.go
@@ -1,5 +1,39 @@
 package main
 
-func main() {
+import (
+	"net/http"
 
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	router := gin.Default()
+
+	router.POST("/SetPipe", func(c *gin.Context) {
+
+		user := c.PostForm("user")
+		node := c.PostForm("node")
+		data := c.PostForm("data")
+
+		SetPipe(user, node, data)
+		c.JSON(http.StatusCreated, gin.H{"ok": "ok"})
+
+	})
+
+	router.GET("/GetPipe", func(c *gin.Context) {
+
+		user := c.Query("user")
+		node := c.Query("node")
+
+		myPipes := GetPipe(user, node)
+		if len(myPipes) == 0 {
+			c.JSON(http.StatusNotFound, gin.H{})
+			return
+		}
+		c.JSON(http.StatusFound, gin.H{"pipes": myPipes})
+	})
+
+	// By default it serves on :8080 unless a
+	// PORT environment variable was defined.
+	router.Run()
 }
