8fe1b4f8441a0735e9c0a1a970e2ce5b744750df
Ram <ram@Apekshas-MacBook-Air.local>
2019-10-19 00:15:05 -0400

    getter

--- pipes.go
+++ pipes.go
@@ -5,6 +5,7 @@ import (
 	"log"
 	"os"
 
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
@@ -13,7 +14,7 @@ const (
 	//Heroku mongo DB name
 	herokuDB = "heroku_q5g3hk72"
 	//Collections
-	pipes = "pipes"
+	pipeCollection = "pipes"
 )
 
 var mongodbURI = os.Getenv("MONGODB_URI") + "?retryWrites=false"
@@ -32,9 +33,9 @@ func SetPipe(user string, node string, data string) {
 	}
 	defer client.Disconnect(context.TODO())
 
-	collection := client.Database(herokuDB).Collection(pipes)
+	pipes := client.Database(herokuDB).Collection(pipeCollection)
 
-	_, err = collection.InsertOne(context.TODO(), pipe{user: user, node: node, data: data})
+	_, err = pipes.InsertOne(context.TODO(), pipe{user: user, node: node, data: data})
 	if err != nil {
 		log.Printf("error: unable to insert to user DB %s", err)
 		return
@@ -43,6 +44,27 @@ func SetPipe(user string, node string, data string) {
 	return
 }
 
-func GetPipe(user string) (myPipes []pipe) {
+func GetPipe(user string, node string) (myPipes []pipe) {
+	client, err := mongo.Connect(context.TODO(), clientOptions)
+	if err != nil {
+		log.Fatal("cannot connect to mongodb")
+	}
+	defer client.Disconnect(context.TODO())
+
+	pipes := client.Database(herokuDB).Collection(pipeCollection)
+	matchingUserAndNode := bson.D{{"user", user}, {"node", node}}
+
+	cur, err := pipes.Find(context.TODO(), matchingUserAndNode)
+	if err != nil {
+		log.Printf("Error on Finding all the documents %s", err)
+	}
+	for cur.Next(context.TODO()) {
+		var p pipe
+		err = cur.Decode(&p)
+		if err != nil {
+			log.Printf("Error on Decoding the document %s", err)
+		}
+		myPipes = append(myPipes, p)
+	}
 	return
 }
