58857d77097cf4b1c516435edc66903982e3823a
Ram Bhat Mulky <rammulky@gmail.com>
2019-10-29 01:58:51 -0400

    statusOK

--- server.go
+++ server.go
@@ -33,7 +33,7 @@ func main() {
 			c.JSON(http.StatusNotFound, gin.H{})
 			return
 		}
-		c.JSON(http.StatusFound, gin.H{"pipes": myPipes})
+		c.JSON(http.StatusOK, gin.H{"pipes": myPipes})
 	})
 
 	// By default it serves on :8080 unless a
